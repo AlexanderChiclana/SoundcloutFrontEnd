df3a743b6fefd859c9e5b9bac194d220b8c6c407
AlexanderChiclana <alexander_chiclana@alumni.brown.edu>
2018-07-25 16:54:04 -0400

    fixed URL

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: ' https://morning-coast-76185.herokuapp.com/',
+  production: ' https://morning-coast-76185.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
